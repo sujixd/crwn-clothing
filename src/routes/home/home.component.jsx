fb1f987892d2a54855d772ed3bfeda25d8ff0b5f
Shubhajit <debnathshubhajit1@gmail.com>
2022-12-15 20:22:10 +0530

    first commit

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import Directory from '../../components/directory/directory.component';
+
+function Home() {
+    return (
+        <div>
+            <Outlet />
+            <Directory />
+        </div>
+    );
+}
+
+export default Home
